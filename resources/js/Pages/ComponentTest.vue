43928dada862b22969b3edba8947b6f4d9077d93
simizu-masato <m.shimizu@trigger4.com>
2023-02-16 16:42:52 +0900

    sec-002 inertia vue

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GestLayout from "@/Layouts/GuestLayout.vue"
+import Label from "@/Components/InputLabel.vue"
+import Input from "@/Components/TextInput.vue"
+
+const emitTest = e => {
+  console.log(e)
+}
+
+</script>
+
+
+<template>
+  <GestLayout>
+    <Label value="件名">タイトル</Label>
+    <Input @update:modelValue="emitTest" modelValue="初期値"></Input>
+  </GestLayout>
+</template>
